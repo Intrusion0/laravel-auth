2c9cb4f4b1cc25fd86c1be75bc68cac91aa712f6
Mario <lombardomario01@gmail.com>
2022-02-02 17:11:03 +0100

    Register added

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,32 +3,32 @@
     
     <div id="app" class="ms-container">
         
+        @auth
+            <div class="ms-container-auth">
+               Ciao {{ Auth::user()->name }}
+            </div>
+        @else
+            <div class="ms-container-auth">
+                Se vuoi continuare, registrati o fai il login
+            </div>
+        @endauth
+
         {{-- Register form --}}
         <h2>REGISTER</h2>
-        <form action="" method="POST">
+        <form action="{{ route('register') }}" method="POST">
+
+            @method('POST')
+            @csrf
 
             <label for="name">Name</label>
-            <input type="text" name="name" placeholder="name">
+            <input type="text" name="name" placeholder="name" value="mario">
             <label for="email">Email</label>
-            <input type="text" name="email" placeholder="email">
+            <input type="text" name="email" placeholder="email" value="mario@gmail.com">
             <label for="password">Password</label>
-            <input type="password" name="password" placeholder="password">
-            <label for="password_confirmation">Confirm password</label>
-            <input type="password" name="password_confirmation" placeholder="password confirmation">
+            <input type="password" name="password" placeholder="password" value="password">
+            <label for="password-confirm">Confirm password</label>
+            <input type="password" name="password_confirmation" placeholder="password confirmation" value="password">
             <input type="submit" class="btn-success" value="REGISTRATI">
-        </form>
-
-        <br><hr class="bg-white"><br>
-
-        {{-- Login form --}}
-        <h2>LOGIN</h2>
-        <form action="" method="POST">
-
-            <label for="email">Email</label>
-            <input type="text" name="email" placeholder="email">
-            <label for="password">Password</label>
-            <input type="password" name="password" placeholder="password">
-            <input type="submit" class="btn-success" value="LOGIN">
         </form>
 
     </div>
